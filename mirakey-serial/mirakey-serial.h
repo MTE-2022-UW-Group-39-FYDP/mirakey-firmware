8cfe322796ee2fa62ca7fdd4303b45d8f121df7d
Francisco Garcia-Gonzalez <fjgarcia@uwaterloo.ca>
2022-02-23 18:58:24 -0500

    some more logic

--- mirakey-serial/mirakey-serial.h
+++ mirakey-serial/mirakey-serial.h
@@ -5,9 +5,11 @@ University of Waterloo Undergraduate Mechatronics Engineering - W2022
 MTE482 Fourth Year Design Project: Mirakey
 */
 
-namespace mirakey_serial {
+#include "main.h"
 
-/* Protocol Interface */
+
+
+/* HAL Equivalent */
 void MKS_Init();
 void MKS_Select(uint8_t SlaveAddress);
 void MKS_TxCommand(uint8_t Command);
@@ -15,4 +17,5 @@ void MKS_TxData(uint8_t * pData, uint16_t Size);
 
 /* Helper Methods */
 static void MKS_GPIO_Init(void);
+void MKS_ActivateDisplay(uint8_t SlaveAddress);
 }
