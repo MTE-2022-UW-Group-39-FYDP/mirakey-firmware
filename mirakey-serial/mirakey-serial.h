ddbaf5d63a9b9c3f4902a4401668849801f66a48
Francisco Garcia-Gonzalez <fjgarcia@uwaterloo.ca>
2022-02-23 23:00:10 -0500

    comments

--- mirakey-serial/mirakey-serial.h
+++ mirakey-serial/mirakey-serial.h
@@ -7,10 +7,21 @@ MTE482 Fourth Year Design Project: Mirakey
 
 #include "main.h"
 
-#ifndef MKS
-#define MKS
+#ifndef MKS_h
+#define MKS_h
 
 #define MKS_NUM_KEYS 12
+/* Layout of the 12 key macro-pad with slave addresses in hex
+
++---+---+---+---+
+| 00| 01| 02| 03|
++---+---+---+---+
+| 04| 05| 06| 07|
++---+---+---+---+
+| 08| 09| 0A| 0B|
++---+---+---+---+
+
+*/
 #define MKS_SPI_PORT hspi2
 #define MKS_DISP_CONTROLLER_SSD1306
 #define MKS_DISP_HEIGHT 40
@@ -22,21 +33,3 @@ void MKS_Init();
 void MKS_TxGlyph(uint8_t SlaveAddress, uint8_t* GlyphBuffer);
 
 #endif
-
-/*
-Notes on firmware design
-
-
-start the DMA
-
-prepare memory to be transfered
-
-one call to MKS for every memory section
-
-MKS begins
-
-CPU is independent at this point
-
-MKS runs through the transaction memory
-
-*/
