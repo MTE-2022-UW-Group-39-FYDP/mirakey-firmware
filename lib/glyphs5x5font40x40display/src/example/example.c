d65f6580c045302afa345435bd6d7df9d15a4319
William Li <williamli5638@gmail.com>
2022-03-18 04:45:55 -0400

    more glyphs

--- lib/glyphs5x5font40x40display/src/example/example.c
+++ lib/glyphs5x5font40x40display/src/example/example.c
@@ -2,12 +2,12 @@
 #include "../glyphs.h"
 
 int main() {
-    unsigned char* bitmap = mapCharToBitmap(' ');
+    unsigned char* bitmap = mapCharToBitmap(146);
     render40x40(bitmap);
-    printf("\n");
-    bitmap = mapCharToBitmap('0');
-    render40x40(bitmap);
-    printf("\n");
-    bitmap = mapCharToBitmap(8);
-    render40x40(bitmap);
+//    printf("\n");
+//    bitmap = mapCharToBitmap('0');
+//    render40x40(bitmap);
+//    printf("\n");
+//    bitmap = mapCharToBitmap(8);
+//    render40x40(bitmap);
 }
